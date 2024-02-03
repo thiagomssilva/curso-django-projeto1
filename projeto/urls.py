ccf135ef7026c901e6e4f9e233e49c2000ec37cc
Thiago Marquetti Sansivieri da Silva <thiagomssilva.ca@gmail.com>
2024-02-03 02:20:32 -0500

    Removed contact and about

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,14 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-from recipes.views import home, contact, about
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('about/', about),
-    path('contact', contact),
+    path('', include("recipes.urls")),
+    path('recipes/', include('recipes.urls'))  # domain.com/recipes/
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("HOME")
-
-
-def contact(request):
-    return HttpResponse("Contact")
-
-
-def about(request):
-    return HttpResponse("About")
+    return render(request, 'recipes/home.html', context={
+        'name': 'Thiago Silva',
+    })
