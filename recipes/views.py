f38cc04249ca64f087d337942a811814cd8496a6
Thiago Marquetti Sansivieri da Silva <thiagomssilva.ca@gmail.com>
2024-02-04 21:18:49 -0500

    Implemented blocks to the html

--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Thiago Silva',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Thiago Silva',
+    })
